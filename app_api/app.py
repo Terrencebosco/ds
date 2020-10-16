39003c84fdddf4093ce14c37fff62a069432a289
Terrence Bosco <terrencebosco@gmail.com>
2020-10-16 19:31:13 -0400

    created model prediction function

--- app_api/app.py
+++ app_api/app.py
@@ -1,7 +1,7 @@
 import flask
 from flask_cors import CORS
 import json #jsonify?, requests?, cors?
-
+from predict import predict_on_new
 def create_app():
     """create instance of our flask app"""
     app = flask.Flask(__name__)
@@ -13,8 +13,11 @@ def create_app():
 
     @app.route('/predict', methods=['POST'])
     def predict_sub():
+        text = request.json['text']
 
-        return(json.dumps({'input':'test', 'predict':'r/AdviceAnimals'}))
+        return(predict_on_new(text))
+
+        # return(json.dumps({'input':'test', 'predict':'r/AdviceAnimals'}))
 
     return app
 
